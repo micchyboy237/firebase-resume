14d8a6c6880accb752f80ff791fe6417f36ef805
Jethro Estrada <jethroestrada237@gmail.com>
2024-04-02 19:49:54 +0800

    Updated resume info

---
+++ api/dataset.js
@@ -0,0 +1,64 @@
+const resumeDataBase = {
+  name: "Jethro Estrada",
+  position: "Full Stack Web / Mobile Developer",
+  summary: `
+    10+ years experience in software development, with a track
+    record of success in delivering client requirements and
+    deploying to App Store and Play Store.
+    Proficient in React, React Native and NodeJS, with extensive
+    experience in end-to-end application development.
+    Goals:
+    - Develop scalable, performance oriented and user friendly
+    web / mobile applications
+    - Streamline development through automation best practices
+    such as Unit / Integration Testing and CI / CD
+    - Continuously learn and stay up to date with company
+    standards, latest technologies and trends
+    - Maintain a growth mindset and continuous improvement with
+    communicating effectively with the team
+    - Strive to contribute to the success of the organization, and
+    improve existing coding standards
+  `,
+  technologies: [
+    {
+      scope: "Frontend",
+      items: [
+        "React",
+        "React Native",
+        "Expo",
+        "GraphQL",
+        "Redux",
+        "Gatsby",
+        "TypeScript",
+        "Vanilla JS / CSS",
+      ],
+    },
+    {
+      scope: "Backend",
+      items: ["NodeJS", "PostgreSQL", "MongoDB", "Firebase", "AWS"],
+    },
+  ],
+};
+
+const resumeDataExperience = [
+  {
+    company: "Macanta Software Ltd. (acquired by JulesAI)",
+    date: "07/2020 - Present",
+    location: "Remote",
+    summary: `
+      Macanta is a white label CRM SaaS that is flexible to fit the needs and
+      requirements of business owners.
+      Key technologies: React, React Native, AWS Lambdas, GraphQL,
+      Serverless, Jest, React Testing Library
+        ○ Sole client side developer for both web and mobile platforms,
+        built from scratch using React, React Native & Apollo GraphQL
+        ○ Sold by founders for a majority stake acquired by JulesAI
+        ○ Architected flexible data schemas and components
+        ○ Currently live and maintaining both web and mobile platforms for
+        over 10 business owners with multiple user daily login /
+        retention
+        ○ Published to App Store and Play Store
+        ○ Partnered with AppSumo
+    `,
+  },
+];
