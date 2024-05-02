4d530baaafb71e962f4d18e93f0dda0e93e5826e
Jethro Estrada <jethroestrada237@gmail.com>
2024-05-03 07:49:25 +0800

    Removed console

--- src/firebaseAnalytics.js
+++ src/firebaseAnalytics.js
@@ -1,7 +1,6 @@
 import ReactGA from 'react-ga4';
 
 const isProduction = process.env.NODE_ENV === 'production';
-console.info('isProduction', isProduction);
 
 const initialize = () => {
   if (isProduction) {
